1368f345598cf9d1ce2f7fa1fbe961d3f79e646b
danrleydaniel <danrleydaniel21@gmail.com>
2020-11-21 10:18:16 -0300

    Adicionados comentários

--- Galaxy pathfinder/personagem.py
+++ Galaxy pathfinder/personagem.py
@@ -18,13 +18,13 @@ class Alien:
 		self.upd = 1
 		self.direc = 0
 
-	def update(self, window_w):
+	def update(self, window_w): #Recebe como parâmetro a largura da tela do jogo
 		if self.x >= window_w - self.w:
-			self.upd = -1
+			self.upd = -1 #Não permite que o alien passe da largura da janela do jogo
 		elif self.x <= 150:
-			self.upd = 1
+			self.upd = 1 #Não permite que o alien passe da largura da janela do jogo
 		self.x += self.upd
 
-		if self.direc == 2:
-			self.direc = 0
-		self.direc += 1
+		if self.direc == 2: #Tem relação com a lista pos_alien no código principal
+			self.direc = 0 #Tem relação com a lista pos_alien no código principal
+		self.direc += 1#Tem relação com a lista pos_alien no código principal
