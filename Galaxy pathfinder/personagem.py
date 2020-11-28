5fb1e6d23effacc63dd82ad72435b087bed93624
danrleydaniel <danrleydaniel21@gmail.com>
2020-11-28 10:11:50 -0300

    Método para matar adicionado na classe Alien

--- Galaxy pathfinder/personagem.py
+++ Galaxy pathfinder/personagem.py
@@ -18,14 +18,22 @@ class Alien:
 		self.wy = wy
 		self.upd = 1
 		self.direc = 0
+		self.vivo = True
 
 	def update(self, window_w): #Recebe como parâmetro a largura da tela do jogo
-		if self.x >= window_w - self.w:
-			self.upd = -1 #Não permite que o alien passe da largura da janela do jogo
-		elif self.x <= 150:
-			self.upd = 1 #Não permite que o alien passe da largura da janela do jogo
-		self.x += self.upd
+		if self.vivo:
+			if self.x >= window_w - self.w:
+				self.upd = -1 #Não permite que o alien passe da largura da janela do jogo
+			elif self.x <= 150:
+				self.upd = 1 #Não permite que o alien passe da largura da janela do jogo
+			self.x += self.upd
 
-		if self.direc == 2: #Tem relação com a lista pos_alien no código principal
-			self.direc = 0 #Tem relação com a lista pos_alien no código principal
-		self.direc += 1#Tem relação com a lista pos_alien no código principal
+			if self.direc == 2: #Tem relação com a lista pos_alien no código principal
+				self.direc = 0 #Tem relação com a lista pos_alien no código principal
+			self.direc += 1 #Tem relação com a lista pos_alien no código principal
+		else:
+			self.upd = 0
+			self.direc = 3
+
+	def matar(self):
+		self.vivo = False
